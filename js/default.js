8b42d04340e2dbde11e452671a39229fc1a3d0c9
clooney007 <clooney_zby@hotmail.com>
2016-07-21 23:41:21 +1200

    1-1

--- js/default.js
+++ js/default.js
@@ -1,57 +1,3 @@
-angular.module('myApp', [])
-	.controller('customersCtrl', function($scope, $http) {
-
-	$http.get("includes/data.php").then(function(response) {
-		$scope.myData = response.data.records;
-	});
-	
-	$scope.reverse = false;
-	$scope.x = 'ID';
-	
-	$scope.sort = function(x) {
-		 $scope.reverse = ($scope.x === x) ? !$scope.reverse : true;
-		 $scope.x = x;
-	}  
-
-	$scope.addPlayer = function() {
-		 var length = $scope.myData.length;
-		 var lastID = $scope.myData[length-1].ID;
-		 var FirstName = $scope.FirstName;
-		 var Surname = $scope.Surname;
-		 var Sex = $scope.Sex;
-		 var Tier = $scope.Tier;
-		 var Email = $scope.Email;
-		 var error = false;
-		 
-		 if(FirstName == undefined) error = true;
-		 if(Surname == undefined) error = true;
-		 if(Sex == undefined || (Sex != 'male' && Sex != 'female')) error = true;
-		 if(Tier == undefined || (Tier !== 'bronze' && Tier !== 'silver' && Tier !== 'gold')) error = true;
-		 if(Email == undefined) error = true;
-		
-		 if(error == false){
-			 $scope.myData.push({
-					ID: Number(lastID) + 1,
-					FirstName: FirstName, 
-					Surname: Surname, 
-					Sex: Sex,
-					Tier: Tier,
-					Email: Email
-			 });
-		 }
-		 
-	 }
-	
-	$scope.setSex = function(sex){
-		$scope.Sex = sex;
-	}
-	
-	$scope.setTier = function(tier){
-		$scope.Tier = tier;
-	}
-    
-});
-
 $(document).ready(function(){
 	$('.table').on('click', 'tr', function(){
 		$('.selected').removeClass('selected');
@@ -65,6 +11,19 @@ $(document).ready(function(){
 	$('.dropdown-tier').on('click', 'li', function(){
 		$('#player-tier').val($('a', this).attr('val'));
 	});
+	
+	BootstrapDialog.alert({
+        title: 'WARNING',
+        message: 'Warning! No Banana!',
+        type: BootstrapDialog.TYPE_WARNING, // <-- Default value is BootstrapDialog.TYPE_PRIMARY
+        closable: true, // <-- Default value is false
+        draggable: true, // <-- Default value is false
+        buttonLabel: 'Roar! Why!', // <-- Default value is 'OK',
+        callback: function(result) {
+            // result will be true if button was click, while it will be false if users close the dialog directly.
+            alert('Result is: ' + result);
+        }
+    });
 
 });
 
