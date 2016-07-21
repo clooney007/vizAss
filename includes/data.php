861e18a8659449ea6358ba53d5d6c88323d84368
clooney007 <clooney_zby@hotmail.com>
2016-07-21 22:54:35 +1200

    bv

---
+++ includes/data.php
@@ -0,0 +1,44 @@
+{ 
+	"records":[ 
+		{
+			"ID":1,
+			"FirstName": "Fred", 
+			"Surname": "Redwood", 
+			"Sex": "male",
+			"Tier": "gold",
+			"Email": "Fred@gmail.com"
+		}, 
+		{
+			"ID":2,
+			"FirstName": "Trevor", 
+			"Surname": "Banforth", 
+			"Sex": "male",
+			"Tier": "gold",
+			"Email": "Trevor@gmail.com"
+		}, 
+		{
+			"ID":3,
+			"FirstName": "James", 
+			"Surname": "Jamerson", 
+			"Sex": "male",
+			"Tier": "gold",
+			"Email": "James@gmail.com"
+		}, 
+		{
+			"ID":4,
+			"FirstName": "Raymond", 
+			"Surname": "Smith", 
+			"Sex": "male",
+			"Tier": "silver",
+			"Email": "Raymond@gmail.com"
+		}, 
+		{
+			"ID":5,
+			"FirstName": "Danny", 
+			"Surname": "Carter", 
+			"Sex": "female",
+			"Tier": "bronze",
+			"Email": "Danny@gmail.com"
+		}
+	] 
+} 
